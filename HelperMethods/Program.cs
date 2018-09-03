0be7110fd5a8d2bc574a2712c417bd43b89cd783
Ryan Wiebe <wieberockin@yahoo.com>
2018-09-02 18:53:48 -0600

    Create a method to simplify the code

--- HelperMethods/Program.cs
+++ HelperMethods/Program.cs
@@ -10,8 +10,12 @@ namespace HelperMethods
     {
         static void Main(string[] args)
         {
+
+            
             Console.WriteLine("The Name Game");
 
+
+            
             Console.Write("What's your first name? ");
             string firstName = Console.ReadLine();
 
@@ -21,6 +25,7 @@ namespace HelperMethods
             Console.Write("In what city were you born?");
             string city = Console.ReadLine();
 
+            /*
             char[] firstNameArray = firstName.ToCharArray();
             Array.Reverse(firstNameArray);
 
@@ -53,10 +58,30 @@ namespace HelperMethods
 
             Console.WriteLine("Results: " + result);
 
+            */
+
+            Console.WriteLine("Results: " );
+            ReverseString(firstName);
+            ReverseString(lastName);
+            ReverseString(city);
+
             Console.ReadLine();
+
+            
 
         }
 
+
+        private static void ReverseString(string message)
+        {
+            char[] messageArray = message.ToCharArray();
+            Array.Reverse(messageArray);
+            foreach (char item in messageArray)
+            {
+                Console.Write(item);
+            }
+            Console.Write(" ");
+        }
 
     }
 }
